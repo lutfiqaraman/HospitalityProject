5a839ee2660ba0cedbd3f1641308040880446e79
lutfiqaraman <lutfiqaraman@gmail.com>
2021-10-29 09:19:29 +0200

    create a recipe item

--- frontend/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from "../../Models/recipe/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  lstRecipe: RecipeModel[] = [
+    new RecipeModel(
+      'Test Recipe',
+      'A test recipe to be displayed',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+  ];
 
   constructor() { }
 
