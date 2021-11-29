4b35b488d2b991e7c3aa77e0bd6e9e09a464bce1
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-29 22:35:21 +0200

    Add ingredients to the shopping list

--- frontend/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ frontend/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(): void {
+
+  }
 }
