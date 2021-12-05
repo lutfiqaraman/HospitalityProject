be5df0c8b5693a981314a60895496799b8019ce7
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-05 19:10:45 +0200

    Add whitespace

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -8,6 +8,7 @@ import {RecipeService} from "./recipe.service";
   styleUrls: ['./recipes.component.css'],
   providers: [ RecipeService ]
 })
+
 export class RecipesComponent implements OnInit {
   selectedRecipe: RecipeModel;
 
