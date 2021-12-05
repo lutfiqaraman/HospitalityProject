80af547b2873b607349f5151c769f7854d0b13ea
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-05 12:36:58 +0200

    edit recipe.service.ts file

--- frontend/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { RecipeModel } from "../../Models/recipe/recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,35 +9,13 @@ import { RecipeModel } from "../../Models/recipe/recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeSelectedByUser = new EventEmitter<RecipeModel>()
+  recipes: RecipeModel[];
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.ListOfRecipes();
+  constructor(private recipeSrv: RecipeService) {
   }
 
-  ListOfRecipes() {
-    let recipes: RecipeModel[] = [];
-
-    recipes.push({
-        name: 'Beef Recipe',
-        description: 'A recipe of beef',
-        imagePath: 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
-    });
-
-    recipes.push({
-      name: 'Chicken Recipe',
-      description: 'A recipe of chicken',
-      imagePath: 'https://cdn.pixabay.com/photo/2018/06/01/20/25/chicken-3447081_1280.jpg'
-    });
-
-    recipes.push({
-      name: 'Salad Recipe',
-      description: 'A recipe of salad',
-      imagePath: 'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg'
-    });
-
-    return recipes;
+  ngOnInit(): void {
+    this.recipes = this.recipeSrv.getRecipes();
   }
 
   onRecipeSelected(recipe: RecipeModel) {
--- frontend/src/app/recipes/recipe.service.ts
+++ frontend/src/app/recipes/recipe.service.ts
@@ -1,3 +1,33 @@
-export class RecipeService {
+import {RecipeModel} from "../Models/recipe/recipe.model";
 
+export class RecipeService {
+  recipes: RecipeModel[] = [];
+
+  createRecipes() {
+
+    this.recipes.push({
+      name: 'Beef Recipe',
+      description: 'A recipe of beef',
+      imagePath: 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    });
+
+    this.recipes.push({
+      name: 'Chicken Recipe',
+      description: 'A recipe of chicken',
+      imagePath: 'https://cdn.pixabay.com/photo/2018/06/01/20/25/chicken-3447081_1280.jpg'
+    });
+
+    this.recipes.push({
+      name: 'Salad Recipe',
+      description: 'A recipe of salad',
+      imagePath: 'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg'
+    });
+
+    return this.recipes;
+  }
+
+  getRecipes() {
+    this.recipes = this.createRecipes();
+    return this.recipes.slice();
+  }
 }
