571991506e663ca036c5e921cd9427ea3d1b5b22
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-07 22:32:02 +0200

    Add create and get ingredients methods

--- frontend/src/app/shopping-list/shopping-list.service.ts
+++ frontend/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,15 @@
+import {IngredientModel} from "../Models/ingredient/ingredient.model";
+
 export class ShoppingListService {
+  private ingredients: IngredientModel[];
 
+  createIngredients() {
+    this.ingredients = [
+      new IngredientModel('Apple', 5),
+      new IngredientModel('Corns', 4)
+    ]
+  }
   getIngredients() {
-
+    this.ingredients.slice();
   }
 }
